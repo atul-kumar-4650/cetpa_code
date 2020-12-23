f30430fd9012676b872bb098ebb3ae3ff304e657
atul kumar <atul23021021@gmail.com>
2020-12-23 20:08:40 +0530

    added program to find size of data type

---
+++ 6_7_2020/Size_find.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Size_find{
+	public static void main(String[] args) {
+		System.out.println("size of int "+(Integer.SIZE)/8);
+		System.out.println("size of float "+Float.SIZE/8);
+		System.out.println("size of double "+Double.SIZE/8);
+		SIZE
+
+	}
+}
