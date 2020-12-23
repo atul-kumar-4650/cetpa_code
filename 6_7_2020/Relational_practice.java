3ebea50204203f08739fb9ceb3ca7cc8fedebccb
atul kumar <atul23021021@gmail.com>
2020-12-23 20:24:45 +0530

    added program  of relational operator

---
+++ 6_7_2020/Relational_practice.java
@@ -0,0 +1,15 @@
+
+
+class Relational_practice{
+	public static void main(String[] args) {
+		int a=1;
+		int b=6;
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a==b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a!=b);
+		
+	}
+}
